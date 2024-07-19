b8f1929457978c4c3798c5a224e752a08504a2d3
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2024-07-19 08:07:45 -0500

    correcion mensaje

--- client/src/pages/admin/Users.jsx
+++ client/src/pages/admin/Users.jsx
@@ -5,7 +5,7 @@ const Users = () => {
       <div className="overlay">
         <h1>Colegio Panamericano Colombo Sueco</h1>
         <p>Celebrando 30 años formando líderes en Cristo para Colombia y las naciones</p>
-        <p className="construction-message">Esta aplicación se en cuentra está en construcción.</p>
+        <p className="construction-message">Esta aplicación se encuentra está en construcción.</p>
       </div>
     </div>
   );
