32fc032a7d7c7ea1feec46022a111f8e82b75f64
CarlosAlfredo4200 <alfredomontoyacorreo2@gmail.com>
2024-09-06 14:43:19 -0500

    QR 4

--- client/src/pages/inventory/QR/QrScanner.jsx
+++ client/src/pages/inventory/QR/QrScanner.jsx
@@ -1,30 +1,19 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { BrowserMultiFormatReader } from '@zxing/browser';
 
 const QrScanner = () => {
   const [scanResult, setScanResult] = useState('');
   const videoRef = useRef(null);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const codeReader = new BrowserMultiFormatReader();
 
     const startScanning = async () => {
       try {
-        // Inicializa la lectura del código QR desde el dispositivo de video
         await codeReader.decodeFromVideoDevice(null, videoRef.current, (result, error) => {
           if (result) {
-            const scannedUrl = result.text; // Aquí tienes el contenido escaneado
-            setScanResult(scannedUrl);
-
-            // Verifica si la URL escaneada coincide con la estructura esperada
-            const relativePath = scannedUrl.replace('http://localhost:3000', ''); // Elimina la parte de la URL base
-            if (relativePath.startsWith('/')) {
-              navigate(relativePath); // Navega a la ruta relativa de la aplicación
-            } else {
-              console.error('URL escaneada no es válida:', scannedUrl);
-            }
+            const scannedText = result.text; // Obtiene el texto del código QR
+            setScanResult(scannedText);
           } else if (error) {
             if (!(error instanceof NotFoundException)) {
               console.error('Error scanning QR code:', error);
@@ -42,15 +31,16 @@ const QrScanner = () => {
     return () => {
       codeReader.reset();
     };
-  }, [navigate]);
+  }, []);
 
   return (
     <div>
       <h3>Escanear QR</h3>
       <video ref={videoRef} style={{ width: '100%' }} autoPlay />
-      {scanResult && <p>Resultado del escaneo: {scanResult}</p>}
+      {scanResult && <h2>ID Escaneado: {scanResult}</h2>}
     </div>
   );
 };
 
 export default QrScanner;
+
